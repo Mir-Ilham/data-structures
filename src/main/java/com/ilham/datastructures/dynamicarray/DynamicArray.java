1239fc4a74915f4963b285ba66cbd33b2f08eb0f
Mir Ilham <mhmilham3@gmail.com>
2022-10-15 22:41:46 +0600

    Create dynamic array class and add size based constructor

--- src/main/java/com/ilham/datastructures/dynamicarray/DynamicArray.java
+++ src/main/java/com/ilham/datastructures/dynamicarray/DynamicArray.java
@@ -1,5 +1,16 @@
 package com.ilham.datastructures.dynamicarray;
 
-public class DynamicArray {
-
+@SuppressWarnings("unchecked")
+public class DynamicArray<T> {
+	private T arr[];
+	private int len = 0;
+	private int capacity = 0;
+	
+	DynamicArray(int capacity) {
+		if (capacity < 0) {
+			throw new IllegalArgumentException("Illegal Capacity: " + capacity);
+		}
+		this.capacity = capacity;
+		arr = (T[]) new Object[capacity];
+	}
 }
